9f3601b15db62964c6025a8e67d376b30a3ac4f4
Christopher Alysson <chrisalysson29@gmail.com>
2025-11-15 19:16:51 -0300

    Jogo Jokenpo
    Jogo criado para o EAD do SENAI sobre POO.

---
+++ jokenpo/Jokenpo/Jokenpo/Player.cs
@@ -0,0 +1,98 @@
+﻿using Jokenpo;
+using System;
+
+namespace Jokenpo
+{
+    public class Player : JokenpoJogo
+    {
+        private Random random = new Random();
+        public int[] escolhaComputador = new int[3];
+        public int[] escolha = new int[3];
+
+        public void Rodadas()
+        {
+            for (int i = 0; i < rodada.Length; i++)
+            {
+                Jogo(i);
+                EscolhaJogador(i);
+                EscolhaComputador(i);
+                Avaliar(i);
+            }
+
+            ResultadoFinal();
+        }
+
+        public void EscolhaJogador(int i)
+        {
+            int escolhaVal;
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (!int.TryParse(input, out escolhaVal) || escolhaVal < 1 || escolhaVal > 3)
+                {
+                    Console.WriteLine("Escolha inválida! Digite 1, 2 ou 3.");
+                    continue;
+                }
+                break;
+            }
+
+            escolha[i] = escolhaVal;
+            if (escolha[i] == 1)
+            {
+                Console.WriteLine("Você escolheu Pedra\n");
+            }
+            else if (escolha[i] == 2)
+            {
+                Console.WriteLine("Você escolheu Papel\n");
+            }
+            else
+            {
+                Console.WriteLine("Você escolheu Tesoura\n");
+            }
+        }
+
+        public void EscolhaComputador(int i)
+        {
+            escolhaComputador[i] = random.Next(1, 4);
+            if (escolhaComputador[i] == 1)
+            {
+                Console.WriteLine("Computador escolheu Pedra\n");
+            }
+            else if (escolhaComputador[i] == 2)
+            {
+                Console.WriteLine("Computador escolheu Papel\n");
+            }
+            else
+            {
+                Console.WriteLine("Computador escolheu Tesoura\n");
+            }
+        }
+
+        private void Avaliar(int i)
+        {
+            int jogador = escolha[i];
+            int cpu = escolhaComputador[i];
+
+            if (jogador == cpu)
+            {
+                Console.WriteLine("Empate nesta rodada!\n");
+                return;
+            }
+            bool jogadorGanha =
+                (jogador == 1 && cpu == 3) ||
+                (jogador == 2 && cpu == 1) ||
+                (jogador == 3 && cpu == 2);
+
+            if (jogadorGanha)
+            {
+                pontuacaoJogador++;
+                Console.WriteLine("Você ganhou esta rodada!\n");
+            }
+            else
+            {
+                pontuacaoComputador++;
+                Console.WriteLine("Computador ganhou esta rodada!\n");
+            }
+        }
+    }
+}
---
+++ jokenpo/Jokenpo/Jokenpo/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Numerics;
+
+namespace Jokenpo
+{
+    public class JokenpoJogo
+    {
+        public int pontuacaoJogador = 0;
+        public int pontuacaoComputador = 0;
+        public int[] rodada = { 1, 2, 3 };
+
+        public void Jogo(int i)
+        {
+            Console.WriteLine("Rodada " + rodada[i] + " - Faça sua escolha:");
+            Console.WriteLine("1 - Pedra");
+            Console.WriteLine("2 - Papel");
+            Console.WriteLine("3 - Tesoura");
+        }
+
+        public void Vitoria()
+        {
+            if (pontuacaoJogador > pontuacaoComputador)
+            {
+                Console.WriteLine("Jogador Venceu!");
+            }
+        }
+
+        public void Derrota()
+        {
+            if (pontuacaoJogador < pontuacaoComputador)
+            {
+                Console.WriteLine("Computador Venceu!");
+            }
+        }
+
+        public void ResultadoFinal()
+        {
+            Console.WriteLine($"\nPlacar final - Jogador: {pontuacaoJogador} | Computador: {pontuacaoComputador}");
+            if (pontuacaoJogador > pontuacaoComputador)
+                Console.WriteLine("Jogador Venceu!");
+            else if (pontuacaoJogador < pontuacaoComputador)
+                Console.WriteLine("Computador Venceu!");
+            else
+                Console.WriteLine("Empate!");
+        }
+    }
+
+    // meu deus do ceu eu redigitei 3x e n sabia que o erro era esse main
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jokenpo - Pedra, Papel, Tesoura\n");
+            var player = new Player();
+            player.Rodadas();
+
+            Console.WriteLine("\nPressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
