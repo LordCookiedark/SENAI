b324572cc02a2ce5c53cbbc2f89aff3805fff5ad
Christopher Alysson <chrisalysson29@gmail.com>
2025-11-23 17:54:13 -0300

    Batalha contra o grande Cthulhu
    Criado uma simulação de batalha usando POO.

---
+++ Battle/Battle/Class1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace Battle
+{
+    public class Characteres
+    {
+        public class Warrior
+        {
+            private int lifeWarrior = 800;
+            public int WarAxes = 0;
+            public int WarShield = 0;
+            public int WarBuff = 2;
+            public bool WarBuffActive = false;
+            private static readonly Random rnd = new Random();
+
+            public void WarShieldBlock(CharacterBoss boss)
+            {
+                Console.WriteLine("O Guerreiro levanta seu escudo para bloquear o ataque...\n");
+                WarShield = rnd.Next(5, 16);
+                if (WarBuffActive && WarBuff >= 0)
+                {
+                    WarShield += WarBuff;
+                    WarBuff--;
+                }
+                boss.ReceiveDamage(WarShield);
+                lifeWarrior += WarShield;
+               
+                Console.WriteLine($"O Guerreiro bloqueou {WarShield} de dano.\n");
+            }
+
+            public void WarAxesAttack(CharacterBoss boss)
+            {
+                Console.WriteLine("O Guerreiro ataca com seu machado...\n");
+                WarAxes = rnd.Next(10, 27);
+                if (WarBuffActive && WarBuff >= 0)
+                {
+                    WarAxes += WarBuff;
+                    WarBuff--;
+                }
+                boss.ReceiveDamage(WarAxes);
+                Console.WriteLine($"O ataque do Guerreiro causou {WarAxes} de dano.\n");
+            }
+
+            public void WarBuffActivate()
+            {
+                WarBuffActive = true;
+                WarBuff = 2;
+                Console.WriteLine("O Guerreiro usou o grito de guerra, buff de habilidade ativo por 2 turnos.\n");
+            }
+
+            public void WarLifeStatus(CharacterBoss boss)
+            {
+                lifeWarrior -= boss.attackBoss;
+                Console.WriteLine($"Vida do Guerreiro: {lifeWarrior}\n");
+                if (lifeWarrior <= 0)
+                {
+                    Console.WriteLine("O Guerreiro foi derrotado!SS\n");
+                }
+            }
+
+            public void ReceiveDamage(int amount)
+            {
+                lifeWarrior -= amount;
+                if (lifeWarrior < 0) lifeWarrior = 0;
+            }
+
+            public void ReceiveHeal(int amount)
+            {
+                lifeWarrior += amount;
+            }
+
+            public int GetLife() => lifeWarrior;
+            public bool IsAlive() => lifeWarrior > 0;
+        }
+
+        public class Mage
+        { 
+            private int lifeMage = 500;
+            public int MagFireball = 0;
+            public int MagWindBurst = 0;
+            public int MagHeal = 0;
+            private static readonly Random rnd = new Random();
+
+            public void MageFireballAttack(CharacterBoss boss)
+            {
+                Console.WriteLine("O Mago lança uma esfera de fogo...\n");
+                MagFireball = rnd.Next(15, 30);
+                boss.ReceiveDamage(MagFireball);
+                if (boss.GetLife() < 0) 
+                    ;
+                Console.WriteLine($"O ataque do Mago causou {MagFireball} de dano.\n");
+            }
+
+            public void MageWindBurstAttack(CharacterBoss boss)
+            {
+                Console.WriteLine("O Mago conjura uma rajada de ventos...\n");
+                MagWindBurst = rnd.Next(8, 25);
+                boss.ReceiveDamage(MagWindBurst);
+                if (boss.GetLife() < 0) 
+                    ;
+                Console.WriteLine($"O ataque do Mago causou {MagWindBurst} de dano.\n");
+            }
+
+            public void MageHealSpell(CharacterBoss boss, Warrior ally)
+            {
+                Console.WriteLine("O Mago usa cura maior...\n");
+                MagHeal = rnd.Next(350, 600);
+                lifeMage += MagHeal;
+                ally.ReceiveHeal(MagHeal);
+                Console.WriteLine($"O Mago recuperou {MagHeal} de vida do time.\n");
+            }
+
+            public void MagLifeStatus(CharacterBoss boss)
+            {
+                lifeMage -= boss.attackBoss;
+                Console.WriteLine($"Vida do Mago: {lifeMage}\n");
+                if (lifeMage <= 0)
+                {
+                    Console.WriteLine("O Mago foi derrotado!\n");
+                }
+            }
+
+            public int GetLife() => lifeMage;
+            public bool IsAlive() => lifeMage > 0;
+        }
+
+    }
+}
---
+++ Battle/Battle/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;  
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Battle
+{
+    public class CharacterBoss
+    {
+        private int lifeBoss = 1300;
+        public int attackBoss = 0;
+        public int heallingBoss = 400;
+        public bool isEnraged = false;
+        public int Enraged = 1;
+        private static readonly Random rnd = new Random();
+        private const int EnrageChancePercent = 50;
+
+        public int lifeStatusBoss()
+        {
+            if(lifeBoss <= 0)
+            {
+                Console.WriteLine("Cthulhu foi derrotado!\nParabens Aventureiro!!");
+            }
+
+
+            if (lifeBoss <= 300 && Enraged == 1 && isEnraged == false)
+            {
+                if (rnd.Next(0, 100) < EnrageChancePercent)
+                {
+                    Console.WriteLine("Cthulhu está enfurecido!\nSeus ataques agora causam mais dano!\nCthulhu recupera 400 de vida.");
+                    isEnraged = true;
+                    lifeBoss += heallingBoss;               
+                    Enraged = 0;
+                }
+            }
+
+            return lifeBoss;
+        }
+        public void ReceiveDamage(int amount)
+        {
+            lifeBoss -= amount;
+            if (lifeBoss < 0) lifeBoss = 0;
+        }
+        public int GetLife() => lifeBoss;
+        public int AttackBoss()
+        {
+            attackBoss = rnd.Next(49, 80);
+            if (isEnraged)
+            {
+                attackBoss += 1800;
+            }
+            if (lifeBoss < 0) lifeBoss = 0;
+            Console.WriteLine($"O ataque de Cthulhu causou {attackBoss} de dano.");
+            return attackBoss;
+        }
+    }
+
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var boss = new CharacterBoss();
+            var warrior = new Characteres.Warrior();
+            var mage = new Characteres.Mage();
+
+            Console.WriteLine("O grande Cthulhu aparece diante de você!");
+            Console.WriteLine("Prepare-se para a batalha!\n");
+            Console.WriteLine("Seu grupo é composto por um guerreiro e um mago\n");
+            Console.WriteLine("Boa sorte Aventureiro!\n");
+            Console.WriteLine($"\nStatus:\n Vida do Guerreiro: {warrior.GetLife()} | Vida do Mago: {mage.GetLife()} | Vida do Chefe: {boss.GetLife()}");
+
+
+
+            while (boss.GetLife() > 0 && (warrior.IsAlive() || mage.IsAlive()))
+            {
+                Console.WriteLine("\n\n\nSeu Guerreiro possui 3 ataques:\n1 - Ataque com o machado.\n2 - Ataque com escudo.\n3 - Grito de guerra.\n");
+                Console.WriteLine("\nEscolha a ação do Guerreiro (1/2/3):\n");
+                var choiceW = Console.ReadLine();
+                switch (choiceW)
+                {
+                    case "1":
+                        warrior.WarAxesAttack(boss);
+                        break;
+                    case "2":
+                        warrior.WarShieldBlock(boss);
+                        break;
+                    case "3":
+                        warrior.WarBuffActivate();
+                        break;
+                    default:
+                        Console.WriteLine("\n\nAção inválida para o Guerreiro. Perdeu o turno.\n\n");
+                        break;
+                }
+                Console.WriteLine("\n\n\nSeu Mago possui 3 ataques:\n1 - Esfera de fogo.\n2 - Rajada de Ventos.\n3 - Cura Maior.\n\n\n");
+                Console.WriteLine("\nEscolha a ação do Mago (1/2/3):\n");
+                var choiceM = Console.ReadLine();
+                switch (choiceM)
+                {
+                    case "1":
+                        mage.MageFireballAttack(boss);
+                        break;
+                    case "2":
+                        mage.MageWindBurstAttack(boss);
+                        break;
+                    case "3":
+                        mage.MageHealSpell(boss, warrior);
+                        break;
+                    default:
+                        Console.WriteLine("\n\nAção inválida para o Mago. Perdeu o turno.\n\n");
+                        break;
+                }
+                boss.lifeStatusBoss();
+
+                if (boss.GetLife() <= 0)
+                    break;
+                boss.AttackBoss();
+                warrior.WarLifeStatus(boss);
+                mage.MagLifeStatus(boss);
+                Console.WriteLine("\nPressione Enter para continuar para o próximo turno...\n\n");
+                Console.ReadLine();
+                Console.Clear(); Console.WriteLine($"\nStatus: Vida do Guerreiro: {warrior.GetLife()} | Vida do Mago: {mage.GetLife()} | Vida do Chefe: {boss.GetLife()}");
+            }
+            if (boss.GetLife() <= 0)
+            {
+                Console.WriteLine("\nParabéns! Você venceu a batalha!");
+            }
+            else
+            {
+                Console.WriteLine("\nSeu time foi derrotado. Fim de jogo.");
+            }
+        }
+    }
+}
